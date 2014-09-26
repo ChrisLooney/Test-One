ca97f7a226df2a842e239a3e951ec2eb7d874646
ChrisLooney <chlooney@stcharlessd.org>
2014-09-26 09:30:27 -0500

    Changed Logic and Syntax

--- Craps_Roller.py
+++ Craps_Roller.py
@@ -1,17 +1,16 @@
 # Craps Roller
 # Demonstrates random number generation
 # Original Author: Michael Dawson
-# Last Edited: Thorin Schmidt
-# Date Edited: 9/26/2014
-
-impart random
+# Last Edited: Chris Looney
+# Date Edited: 9/26/2014, 09:30
+import random
 
 # generate random numbers 1 - 6
-die1 = random.randint(1, 6) 
-die2 = random.randrange(6)
+die1 = random.randint(1,6) 
+die2 = random.randint(1,6)
 
-total = 'die1' + 'die2'
+total = print(die1 + die2)
 
 print("You rolled a", die1, "and a", die2, "for a total of", total)
 
-input("\n\nPress the enter key to exit.")
+input("Press the enter key to exit.")
